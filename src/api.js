f5167eb290836db7f5a6d0d9dfa6db9b5128d066
sdmlr <sdmlr_@hotmail.com>
2024-11-21 15:54:43 +0000

    screen with fetching data enable

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiBase = "https://backend-for-trippy.onrender.com/api";
+
+export const fetchTrips = () => {
+  return axios.get(`${apiBase}/trips`);
+};
+
+export const fetchUserTrips = (userId) => {
+  return axios.get(`${apiBase}/users/${userId}/trips`);
+};
+
+export const fetchTripById = (tripId) => {
+    return axios.get(`${apiBase}/trips/${tripId}`)
+}
