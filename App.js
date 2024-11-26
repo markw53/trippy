c628c7d19ec9c8dbb9878e11355b65257fb37588
sdmlr <sdmlr_@hotmail.com>
2024-11-26 17:26:10 +0000

    screen back to normal after fixing mainapp file

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import "react-native-get-random-values";
 import React from "react";
 import { AuthProvider } from "./AuthContext";
-
 import MainApp from "./MainApp";
-import AddMembersScreen from "./src/screens/AddMembersScreen";
 
 export default function App() {
   return (
     <AuthProvider>
-      {/* <MainApp /> */}
-      <AddMembersScreen />
+      <MainApp />
     </AuthProvider>
   );
 }
