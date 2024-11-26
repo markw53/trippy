c74bdb89d18dee46b0139d829d157e044feb6bad
serk03 <onalserkan@googlemail.com>
2024-11-26 12:32:58 +0000

    Trip Details/ Add Members screen layout need  a few adjustments, get trip details and patch trip details are functional. View and Add members to trip functionality needs to be implemented.

--- src/screens/AddMembersScreen.js
+++ src/screens/AddMembersScreen.js
@@ -19,28 +19,32 @@ import Card from "../components/Card";
 export default function AddMembersScreen() {
     const [tripName, setTripName] = useState("");
     const [tripPic, setTripPic] = useState("");
+    const [tripDescription, setTripDescription] = useState("");
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
 
-    const [originalTripName, setOriginalTripName] = useState()
-    const [originalTripPic, setOriginalTripPic] = useState()
+    const [originalTripName, setOriginalTripName] = useState("")
+    const [originalTripPic, setOriginalTripPic] = useState("")
+    const [originalTripDescription, setOriginalTripDescription] = useState("")
 
-    const tripId = 9;
+    const tripId = 8;
 
-    // const isFormValid = tripName.trim() !== "" && tripPic.trim() !== "";
+    const isFormValid = tripName.trim() !== "" && tripPic.trim() !== "";
 
     const handleSubmit = () => {
         const updateTrip = {
-            trip_id: trip_id,
+            trip_id: tripId,
             trip_name: tripName,
             trip_img_url: tripPic,
+            description: tripDescription,
         }
         patchTripDetails(updateTrip)
             .then((response) => {
                 const updatedTrip = response.data.trip;
 
-                setOriginalTripName(updatedTrip.tripName || tripName);
+                setOriginalTripName(updatedTrip.trip_name || tripName);
                 setOriginalTripPic(updatedTrip.trip_img_url || tripPic);
+                setOriginalTripDescription(updateTrip.description || tripDescription)
 
                 alert("Trip details updated successfully!");
             })
@@ -54,6 +58,8 @@ export default function AddMembersScreen() {
         // Revert to original values
         setTripName(originalTripName);
         setTripPic(originalTripPic);
+        setOriginalTripDescription(originalTripDescription)
+
     };
 
     useEffect(() => {
@@ -61,12 +67,15 @@ export default function AddMembersScreen() {
             .then((response) => {
                 const data = response.data.trip;
 
-                // console.log(data);
+                // console.log("Fetched trip data:", data);
+
                 setTripName(data.trip_name || "");
                 setTripPic(data.trip_img_url || "");
+                setTripDescription(data.description || "");
 
-                setOriginalTripName(data.tripName || "");
-                setOriginalTripPic(data.trip_img_url || "")
+                setOriginalTripName(data.trip_name || "");
+                setOriginalTripPic(data.trip_img_url || "");
+                setOriginalTripDescription(data.description || "");
 
                 setIsLoading(false)
             })
@@ -101,12 +110,12 @@ export default function AddMembersScreen() {
                         <Image
                             style={styles.image}
                             source={{
-                                uri: tripPic || "https://static.independent.co.uk/2023/08/31/12/iStock-1484200613.jpg"
+                                uri: tripPic || "https://reactnative.dev/img/tiny_logo.png"
                             }}
                             onError={(e) => {
                                 console.error("Image failed to load:", e.nativeEvent.error);
                                 if (!originalTripPic) {
-                                    setOriginalTripPic("https://reactnative.dev/img/tiny_logo.png");
+                                    setTripPic("https://reactnative.dev/img/tiny_logo.png");
                                 }
                             }}
                         />
@@ -118,7 +127,7 @@ export default function AddMembersScreen() {
                         <TextInput
                             style={styles.input}
                             onChangeText={setTripName}
-                            // value={tripName}
+                            value={tripName}
                             numberOfLines={1}
                             multiline={false}
                             textAlignVertical="center"
@@ -126,12 +135,26 @@ export default function AddMembersScreen() {
                         />
                     </View>
                     <View>
-                        <Text style={styles.label}>Change Trip  Picture:</Text>
+                        <Text style={styles.label}>Change Trip Picture:</Text>
                         <TextInput
                             style={styles.input}
-                            onChange={setTripPic}
+                            onChangeText={setTripPic}
+                            value={tripPic}
                             numberOfLines={1}
                             multiline={false}
+                            textAlignVertical="center"
+                            autoCapitalize="none"
+                        />
+                    </View>
+                    <View>
+                        <Text style={styles.label}>Change Trip Description:</Text>
+                        <TextInput
+                            style={[styles.input, styles.tripDescription]}
+                            onChangeText={setTripDescription}
+                            value={tripDescription}
+                            numberOfLines={5}
+                            scrollEnabled={true}
+                            multiline={true}
                             textAlignVertical="center"
                             autoCapitalize="none"
                         />
@@ -150,22 +173,30 @@ export default function AddMembersScreen() {
                             textAlignVertical="center"
                             autoCapitalize="none"
                         />
-                        <Button title="Add Member"
-                            style={styles.button}
-                            textStyle={styles.buttonText}
-                        />
+                        <View style={styles.addMemberbtn}>
+                            <Button title="Add Member"
+                                style={styles.button}
+                            />
+                        </View>
                     </View>
                     <View style={styles.confirmationbtnsContainer}>
-                        <Button title="Save Changes"
-                            style={[styles.button, styles.confirmationbtn]}
-                            textStyle={styles.buttonText}
-
-                        />
-                        <Button title="Cancel"
-                            style={[styles.button, styles.confirmationbtn]}
-                            textStyle={styles.buttonText}
-
-                        />
+                        <View style={styles.saveChangesbtn}>
+                            <TouchableOpacity
+                                onPress={handleSubmit}
+                                disabled={!isFormValid}
+                                style={[
+                                    styles.button,
+                                    !isFormValid && styles.buttonDisabled,
+                                ]}
+                            >
+                                <Text style={styles.buttonText}>Save Changes</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.cancelbtn}>
+                            <Button title="Cancel"
+                                onPress={handleCancel}
+                            />
+                        </View>
                     </View>
                 </ScrollView>
             </View>
@@ -182,14 +213,80 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        padding: 30,
+        padding: 20,
     },
     text: {
         marginBottom: 30,
         fontSize: 24,
         color: "#24565C",
         fontWeight: "bold",
-        textAlign: "center"
+    },
+    label: {
+        fontSize: 16,
+        marginVertical: 10,
+        color: "#333",
+    },
+    input: {
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        padding: 12,
+        fontSize: 16,
+    },
+    imageContainer: {
+        alignItems: "center",
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+    },
+    userNameheading: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginTop: 10,
+    },
+    addMemberbtn: {
+        padding: 10,
+    },
+    saveChangesbtn: {
+        padding: 10,
+    },
+    cancelbtn: {
+        padding: 10,
+    },
+    loadingText: {
+        fontSize: 18,
+        color: "#999",
+        marginTop: 20,
+    },
+    disabledInput: {
+        backgroundColor: "#f5f5f5",
+        color: "#999",
+    },
+    button: {
+        backgroundColor: "#24565C",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 25,
+        alignItems: "center",
+    },
+    buttonDisabled: {
+        backgroundColor: "#ccc",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 25,
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    confirmationbtnsContainer: {
+        marginTop: 50,
+        color: "#fff",
     },
     tripNameheading: {
         fontSize: 18,
@@ -213,54 +310,108 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textAlign: "center"
     },
-    label: {
-        paddingHorizontal: 50,
-        fontSize: 16,
-        marginVertical: 10, // Keep vertical spacing
-        color: "#333",
-        textAlign: "left", // Align the text to the left
-        alignSelf: "flex-start", // Align the label itself to the start of its parent container
-    },
-    input: {
-        backgroundColor: "#fff",
-        borderRadius: 10,
-        borderColor: "#ccc",
-        borderWidth: 1,
-        marginBottom: 10,
-        padding: 12,
-        fontSize: 16,
-        width: "100%",
-    },
-    imageContainer: {
-        alignItems: "center",
-    },
-    image: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-    },
-    button: {
-        backgroundColor: "#24565C",
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 25,
-        alignItems: "center",
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: 16,
-        fontWeight: "bold",
-    },
-    confirmationbtnsContainer: {
-        marginTop: 50,
-        color: "#fff",
-    },
-    confirmationbtn: {
-        marginTop: 20
-    },
-    loadingText: {
-        fontSize: 18,
-        color: "#999",
-        marginTop: 20,
-    },
+
+    // container: {
+    //     flex: 1,
+    //     backgroundColor: "#F7F7F7",
+    // },
+    // content: {
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     padding: 30,
+    // },
+    // text: {
+    //     marginBottom: 30,
+    //     fontSize: 24,
+    //     color: "#24565C",
+    //     fontWeight: "bold",
+    //     textAlign: "center"
+    // },
+    // tripNameheading: {
+    //     fontSize: 18,
+    //     fontWeight: "bold",
+    //     marginTop: 20,
+    //     marginBottom: 20,
+    // },
+    // textCurrentMembers: {
+    //     marginTop: 20,
+    //     marginBottom: 10,
+    //     fontSize: 20,
+    //     color: "#24565C",
+    //     fontWeight: "bold",
+    //     textAlign: "center"
+    // },
+    // textAddMembers: {
+    //     marginTop: 20,
+    //     marginBottom: 5,
+    //     fontSize: 20,
+    //     color: "#24565C",
+    //     fontWeight: "bold",
+    //     textAlign: "center"
+    // },
+    // label: {
+    //     paddingHorizontal: 50,
+    //     fontSize: 16,
+    //     marginVertical: 10, // Keep vertical spacing
+    //     color: "#333",
+    //     textAlign: "left", // Align the text to the left
+    //     alignSelf: "flex-start", // Align the label itself to the start of its parent container
+    // },
+    // input: {
+    //     backgroundColor: "#fff",
+    //     borderRadius: 10,
+    //     borderColor: "#ccc",
+    //     borderWidth: 1,
+    //     marginBottom: 10,
+    //     padding: 12,
+    //     fontSize: 16,
+    //     width: "100%",
+    // },
+    // imageContainer: {
+    //     alignItems: "center",
+    // },
+    // image: {
+    //     width: 100, // Ensure non-zero values
+    //     height: 100,
+    //     borderRadius: 50,
+    //     backgroundColor: "#ccc", // Optional: Placeholder background
+    // },
+    // button: {
+    //     backgroundColor: "#24565C",
+    //     paddingVertical: 10,
+    //     paddingHorizontal: 20,
+    //     borderRadius: 25,
+    //     alignItems: "center",
+    // },
+    // buttonText: {
+    //     color: "#fff",
+    //     fontSize: 16,
+    //     fontWeight: "bold",
+    // },
+    // confirmationbtnsContainer: {
+    //     marginTop: 50,
+    //     color: "#fff",
+    // },
+    // confirmationbtn: {
+    //     marginTop: 20
+    // },
+    // loadingText: {
+    //     fontSize: 18,
+    //     color: "#999",
+    //     marginTop: 20,
+    // },
+    // buttonDisabled: {
+    //     backgroundColor: "#ccc",
+    //     paddingVertical: 10,
+    //     paddingHorizontal: 20,
+    //     borderRadius: 25,
+    //     alignItems: "center"
+    // },
+    // saveChangesbtn: {
+    //     padding: 10,
+    // },
+    // cancelbtn: {
+    //     padding: 10,
+    // },
 });
